e75fa4cd31fbee49ea28d7ef002b87764476c0f7
FyuroTEX <yurilev4@gmail.com>
2020-06-10 02:35:46 +0300

    babel-react

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <p>Hello</p>;
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
 
 
 
-class App {
+// class App {
 
-    run = async (name = 'World') => {
-        console.log(`Hello ${name}`);
-        console.log([1, 2, [2, 3]].flat());
-    };
+//     run = async (name = 'World') => {
+//         console.log(`Hello ${name}`);
+//         console.log([1, 2, [2, 3]].flat());
+//     };
 
-};
-const app = new App();
-app.run()
-    .then(() => console.log('done'))
-    .catch(()=> console.log('Error'));
+// };
+// const app = new App();
+// app.run()
+//     .then(() => console.log('done'))
+//     .catch(()=> console.log('Error'));
