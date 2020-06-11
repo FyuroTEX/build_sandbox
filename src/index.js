f5197a6b0c2caefc9c366e892ea91836d729fdc3
FyuroTEX <yurilev4@gmail.com>
2020-06-11 15:24:35 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import Log from './log';
-import Calc from './calc';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import img from './ger.png';
+import './main.css';
 
-const calc = new Calc();
-const log = new Log();
+const App = () => {
+    return <p>Hello</p>;
+};
 
-log.log(calc.add(1, 2, 3));
-
-const el = document.createElement('img');
-el.src = img;
-document.body.appendChild(el);
+ReactDOM.render(<App />, document.getElementById('root'));
