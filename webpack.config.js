80ae6cc3afe18cb19b765ac03ed71aa66284d360
FyuroTEX <yurilev4@gmail.com>
2020-06-17 03:51:06 +0300

    webpack prod and dev configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,87 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtructPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {//Loading images
-                test: /\.(png|jpg|jpeg|gif|ico)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images',
-                            name: '[name]-[sha1:hash:7].[ext]'
-                        }
-                    }
-                ]
-            },
-            {//Loading fonts
-                test: /\.(ttf|otf|eot|woff|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'fonts',
-                            name: '[name].[ext]'
-                        }
-                    }
-                ]
-            },
-            {//Loafing CSS
-                test: /\.(css)$/,
-                use: [MiniCssExtructPlugin.loader, 'css-loader']
-            },
-            {//Loading SASS/SCSS
-                test: /\.(s[ac]ss)$/,
-                use: [MiniCssExtructPlugin.loader, 'css-loader',
-                    { loader: 'sass-loader', options: {} },
-                ]
-            },
+module.exports = (env = {}) => {
+
+    const { mode = 'development' } = env;
+    const isProd = mode === 'production';
+    const isDev = mode === 'development';
+
+    const getStyleLoaders = () => {
+        return [
+            isProd ? MiniCssExtructPlugin.loader : 'style-loader',
+            'css-loader'
+        ];
+    };
+
+    const getPlugins = () => {
+        const plugins = [
+            new HtmlWebpackPlugin({
+                title: 'Build Webpack Sandbox',
+                buildTime: new Date().toISOString(),
+                template: 'public/index.html'
+            }),
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Build Webpack Sandbox',
-            buildTime: new Date().toISOString(),
-            template: 'public/index.html'
-        }),
-        new MiniCssExtructPlugin({
-            filename: 'main-[hash:8].css'
-        })
-    ],
-    devServer: {
-        open: true
-    }
-}
+        if (isProd) {
+            plugins.push(
+                new MiniCssExtructPlugin({
+                    filename: 'main-[hash:8].css'
+                })
+            );
+        };
+        return plugins;
+    };
+
+    return {
+        mode: isProd ? 'production' : isDev && 'development',
+        output: {
+            filename: isProd ? 'main-[hash:8].js' : undefined
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {//Loading images
+                    test: /\.(png|jpg|jpeg|gif|ico)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name]-[sha1:hash:7].[ext]'
+                            }
+                        }
+                    ]
+                },
+                {//Loading fonts
+                    test: /\.(ttf|otf|eot|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'fonts',
+                                name: '[name].[ext]'
+                            }
+                        }
+                    ]
+                },
+                {//Loafing CSS
+                    test: /\.(css)$/,
+                    use: getStyleLoaders()
+                },
+                {//Loading SASS/SCSS
+                    test: /\.(s[ac]ss)$/,
+                    use: [...getStyleLoaders(), { loader: 'sass-loader', options: {} }
+                    ]
+                },
+            ]
+        },
+        plugins: getPlugins(),
+        devServer: {
+            open: true
+        }
+    };
+};
